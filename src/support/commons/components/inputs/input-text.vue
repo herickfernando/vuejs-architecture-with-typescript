29d914cf846ea96327f3c402a6f5ee02d52e825b
Herick Fernando Borgo <herick.borgo@uds.com.br>
2019-05-26 23:32:09 -0300

    Added input component.

---
+++ src/support/commons/components/inputs/input-text.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  input.input(
+    name="input"
+    :placeholder="placeholder"
+  )
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Input extends Vue {
+  @Prop() private placeholder!: string;
+}
+</script>
+
+<style lang="scss">
+  .input {
+    width: 15vh;
+    height: 3vh;
+    border: .2vh solid grey;
+    border-radius: 1vh;
+    font-family: 'Times New Roman', Times, serif;
+    font-size: 15px;
+    &:focus {
+      border: .1vh solid grey;
+      box-shadow: .2vh .2vh .3vh;
+      outline: none;
+    }
+  }
+</style>
--- src/views/About/About.vue
+++ src/views/About/About.vue
@@ -1,4 +1,17 @@
 <template lang="pug">
   .about
     h1 This is an about page
+    input-text(placeholder="Name")
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import InputText from '@/support/commons/components/inputs/input-text.vue';
+
+@Component({
+  components: {
+    InputText,
+  },
+})
+export default class About extends Vue {}
+</script>
